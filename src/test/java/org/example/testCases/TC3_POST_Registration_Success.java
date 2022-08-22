ab882643c5211b36cd7868b977efbc114f8d5358
Aleksey Tarasov <coolerr195@gmail.com>
2022-08-22 11:59:10 +0300

    add new testCase TC3_POST_Registration_Success

---
+++ src/test/java/org/example/testCases/TC3_POST_Registration_Success.java
@@ -0,0 +1,66 @@
+package org.example.testCases;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import org.assertj.core.api.Assertions;
+import org.example.base.TestBase;
+import org.example.propertyUtils.PropertyUtils;
+import org.json.simple.JSONObject;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TC3_POST_Registration_Success extends TestBase {
+
+    @BeforeClass
+    public void postRegesterUserSuccess(){
+
+        logger.info("*** Started TC3_POST_Registration_Success ****");
+        RestAssured.baseURI = PropertyUtils.getValue("base_url");
+
+        httpRequest = RestAssured
+                .given()
+                .header("Content-Type", "application/json");
+
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("email", "eve.holt@reqres.in");
+        requestParams.put("password", "pistol");
+
+        httpRequest.body(requestParams.toJSONString());
+
+        response = httpRequest.request(Method.POST, "api/register");
+    }
+
+    @Test
+    public void TC3CheckStatusCode(){
+        logger.info("*** Checking Status Code ***");
+        int statusCode = response.getStatusCode();
+        logger.info("Status code is " + statusCode);
+        Assertions.assertThat(statusCode)
+                .isEqualTo(200);
+    }
+
+    @Test
+    public void TC3CheckHeaders(){
+        logger.info("*** Checking headers ***");
+        String contentType = response.header("Content-Type");
+        logger.info("Content-type header is " + contentType);
+        Assertions.assertThat(contentType)
+                .isNotNull()
+                .isEqualTo("application/json; charset=utf-8");
+    }
+
+    @Test
+    public void TC1JSONValidation() {
+        logger.info("*** Checking JSON body values ***");
+        int idValue = response.getBody().jsonPath().getInt("id");
+        logger.info("*** Response value for 'id' is " + idValue);
+        Assertions.assertThat(idValue)
+                .isBetween(1, 10000);
+
+        String tokenValue = response.getBody().jsonPath().getString("token");
+        logger.info("*** Response value for 'token' is " + tokenValue);
+        Assertions.assertThat(tokenValue)
+                .isNotNull()
+                .hasSize(17);
+    }
+}
